96874e620b02d8c0e45c954f86e1293b7ff652d9
ak3489 <saadmin@126.com>
2015-04-29 11:33:30 +0800

    add css animate

--- js/common.js
+++ js/common.js
@@ -2,18 +2,18 @@ var h = $(window).height();
 $('.wrap').css('height', h);
 $('.shade').css('height', h);
 $('.col').hover(function(){
-	var bp = $(this).css("background-position");
+//	var bp = $(this).css("background-position");
 //	alert(bp)
 	$(this).find('.shade').css('display','none');
-	$(this).stop().animate({
-		backgroundPosition:bp-5+'px'
-	});
+//	$(this).stop().animate({
+//		backgroundPosition:bp-5+'px'
+//	});
 	$(this).find('p').css({'background':'rgba(0,0,0,.5)','margin-right':'2em'})
 },function(){
 	$(this).find('.shade').css('display','block');
-	$(this).stop().animate({
-		backgroundPosition:bp+'px'
-	});
+//	$(this).stop().animate({
+//		backgroundPosition:bp+'px'
+//	});
 	$(this).find('p').css({'background':'none','margin-right':'2em'})
 });
 
